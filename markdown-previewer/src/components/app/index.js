079670958345b7f07cc2b1be7374a9c8a4a11348
Alexander <alexandr.bbm@gmail.com>
2016-08-30 18:21:03 +0800

    Init

---
+++ markdown-previewer/src/components/app/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    state = {
+        news: '',
+    };
+
+    render () {
+        return (
+            <div className="app">
+                <h3>My react boilerplate</h3>
+            </div>
+        )
+    }
+
+}
