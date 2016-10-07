41cdbc785b99595d91ca3f6432e6b2d16ac78349
Alexander <alexandr.bbm@gmail.com>
2016-10-07 10:32:25 +0700

    scatterplot-graph added bg

--- d3-projects/scatterplot-graph/script.js
+++ d3-projects/scatterplot-graph/script.js
@@ -1,10 +1,10 @@
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
+var margin = {top: 20, right: 20, bottom: 50, left: 50},
+    width = 900 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 // parse the date / time
-var parseTime = d3.timeParse("%d-%b-%y");
+var parseTime = d3.timeParse('%d-%b-%y');
 
 // set the ranges
 var x = d3.scaleLinear().range([0, width]);
@@ -13,12 +13,12 @@ var y = d3.scaleLinear().range([0, height]);
 // append the svg obgect to the body of the page
 // appends a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
-var svg = d3.select("#scatterplot-graph").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select('#scatterplot-graph').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform',
+        'translate(' + margin.left + ',' + margin.top + ')');
 
 var dataUrl = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
 function getData () {
@@ -36,35 +36,63 @@ getData().then((data) => {
     y.domain([1, d3.max(data, (d) => d.Place) + 1]);
 
 
-    var div = d3.select("#scatterplot-graph").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
+    var tooltip = d3.select('#scatterplot-graph').append('div')
+        .attr('class', 'tooltip')
+        .style('opacity', 0);
 
     // add the dots
-    svg.selectAll("dot")
+    svg.selectAll('dot')
         .data(data)
-        .enter().append("circle")
-        .attr("r", 5)
-        .attr("cx", (d) => x(d.Seconds))
-        .attr("cy", (d) => y(d.Place))
-        .on("mouseover", (d) => {
-            div.transition()
+        .enter().append('circle')
+        .attr('r', 5)
+        .attr('cx', (d) => x(d.Seconds))
+        .attr('cy', (d) => y(d.Place))
+        .on('mouseover', function (d) {
+            d3.select(this).transition()
+                .duration(500)
+                .attr('r', 10);
+
+            tooltip.transition()
                 .duration(200)
-                .style("opacity", .9);
-            div.html('Doping info: ' + d.Doping)
+                .style('opacity', .9);
+            tooltip.html('Doping info: ' + d.Doping)
         })
-        .on('mouseout', () => {
-            div.transition()
+        .on('mouseout', function () {
+            tooltip.transition()
                 .duration(200)
-                .style("opacity", 0);
+                .style('opacity', 0);
+            d3.select(this).transition()
+                .duration(500)
+                .attr('r', 5);
         });
 
+    svg.append('text')
+        .attr('transform',
+            'translate(' + (width/2) + ' ,' +
+            (height + margin.top + 20) + ')')
+        .style('text-anchor', 'middle')
+        .text('Seconds');
+
+    // text label for the y axis
+    svg.append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 0 - margin.left)
+        .attr('x',0 - (height / 2))
+        .attr('dy', '1em')
+        .style('text-anchor', 'middle')
+        .text('Place');
+
+
+
     // add the X Axis
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
+    svg.append('g')
+        .attr('transform', 'translate(0,' + height + ')')
         .call(d3.axisBottom(x));
 
     // add the Y Axis
-    svg.append("g")
-        .call(d3.axisLeft(y));
+    svg.append('g')
+        .call(d3.axisLeft(y).tickValues([1, 5, 10, 15, 20, 25, 30, 35]) );
+
+    // text label for the x axis
+
 });
